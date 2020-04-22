761d600f14db39fdb00334c0fb8c3d43f951970e
Rachel Finucane <rachelkfinucane@gmail.com>
2020-04-22 09:13:01 +0100

    added fields, getters and setters, constructors

---
+++ java/src/ie/tudublin/Task.java
@@ -0,0 +1,49 @@
+package ie.tudublin;
+
+import processing.data.TableRow;
+
+public class Task
+{
+    private String name;
+    private int startDate;
+    private int endDate;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(int startDate) {
+        this.startDate = startDate;
+    }
+
+    public int getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(int endDate) {
+        this.endDate = endDate;
+    }
+
+    public Task(String name, int startDate, int endDate)
+    {
+        this.name = name;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    public Task(TableRow tr)
+    {
+        this(tr.getString("Task"), tr.getInt("Start"), tr.getInt("End"));
+        
+    }
+    
+
+}
