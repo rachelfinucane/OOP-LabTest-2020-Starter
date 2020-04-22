60bbba4c483f4082d4b7d37c4429955bf4b52b6b
Rachel Finucane <rachelkfinucane@gmail.com>
2020-04-22 10:00:50 +0100

    added displayTasks() to Gantt.java

--- java/src/ie/tudublin/Gantt.java
+++ java/src/ie/tudublin/Gantt.java
@@ -33,6 +33,46 @@ public class Gantt extends PApplet
 			println(t);
 		}
 	}
+
+	public void displayTasks()
+	{
+		float leftMargin = width / 6;
+		float margin = width / 20;
+		int numDays = 30;
+		float x, y;
+		float rHeight = 50;
+		float rStart, rEnd, rWidth;
+		float color;
+
+		stroke(255);
+		fill(255);
+		textAlign(CENTER);
+
+		for(int i = 1; i <= numDays; i++)
+		{
+			x = map(i, 1, 30, leftMargin, width - margin);
+			line(x, margin, x, height - margin);
+
+			text(i, x, margin * 0.8f);
+		}
+
+		for(int i = 0; i < tasks.size(); i++)
+		{
+			fill(255);
+			y = map(i, 0, tasks.size(), 2 * margin, height - margin);
+			text(tasks.get(i).getName(), margin, y);
+
+			noStroke();
+			color = map(i, 0, tasks.size(), 0, 255);
+			fill(color, 255, 255);
+			
+			rStart = map(tasks.get(i).getStartDate(), 1, 30, leftMargin, width - margin);
+			rEnd = map(tasks.get(i).getEndDate(), 1, 30, leftMargin, width - margin);
+			rWidth = rEnd - rStart;
+
+			rect(rStart, y - rHeight / 2, rWidth, rHeight);
+		}
+	}
 	
 	public void mousePressed()
 	{
@@ -55,6 +95,8 @@ public class Gantt extends PApplet
 	
 	public void draw()
 	{			
+		colorMode(HSB);
 		background(0);
+		displayTasks();
 	}
 }
